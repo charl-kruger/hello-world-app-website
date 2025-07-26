f4d35e15478e4f63a323b92a73fa66e5d24942b5
Charl Kruger <nit3watch@gmail.com>
2025-07-26 16:03:08 +0200

    Delete app directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <h1 className="text-4xl md:text-6xl font-bold text-gray-900 tracking-tight">Hello</h1>
-    </div>
-  )
-}
